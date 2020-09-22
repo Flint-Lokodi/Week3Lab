71d143e56b970fe5c7f0e1f65744f023977449fd
608787 <608787@SAIT224213IT.ACDM.DS.SAIT.CA>
2020-09-22 17:45:23 -0600

    Added functionality to the page and verification checking No link to the Arithmetic Calculator Yet

--- src/java/servlets/AgeCalculatorServlet.java
+++ src/java/servlets/AgeCalculatorServlet.java
@@ -22,7 +22,33 @@ public class AgeCalculatorServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, NumberFormatException {
+       String ageString = request.getParameter("currentAge");
+       if(ageString == null){
+           String nullAge = "You must give your current age";
+           request.setAttribute("nullAge", nullAge);
+           getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                .forward(request, response);
+       }
+       try{
+           int currentAge = Integer.parseInt(ageString);
+           int nextYearAge = currentAge+1;
+           String correct = "Your age next year will be ";
+           request.setAttribute("correct", correct);
+           request.setAttribute("nextYearAge", nextYearAge);
+           getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                .forward(request, response);
+       }catch (NumberFormatException e)
+       {
+           String wrong = "You must enter a number.";
+           request.setAttribute("wrong", wrong);
+           getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                .forward(request, response);
+       }
+       
+       
+       
+       
        
     }
 
--- web/WEB-INF/agecalculator.jsp
+++ web/WEB-INF/agecalculator.jsp
@@ -15,8 +15,10 @@
         <h1>Age Calculator</h1>
         <form method="post" action="age">
             Enter your age: <input type="text" name="currentAge"><br>
-            <input type="button" value="Age next birthday"><br>
+            <input type="submit" value="Age next birthday">
+            </form>
+            ${correct}${nextYearAge}${wrong}${nullAge}<br>
             <a href="url">Arithmetic Calculator</a>
-        </form>
+        
     </body>
 </html>
